5984d88fd132bf43896713fa12dbae12cc3869d7
Rainy-Memory <2630737606@qq.com>
2022-05-02 17:06:42 +0800

    add license

--- RineLab.c
+++ RineLab.c
@@ -1,3 +1,25 @@
+/*
+  RineLab: a simple Instant Messager based on FUSE
+  Copyright 2022 Rainy Memory
+  
+  This program can be distributed under the terms of the GNU GPL.
+  See the file COPYING.
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #define FUSE_USE_VERSION 31
 
 #include <fuse.h>
